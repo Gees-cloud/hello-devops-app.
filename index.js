1719a2a7290fd0bf86bc758f8ae7ec58b5c83ff3
Gloria Ejike <ejikegloria20@gmail.com>
2025-07-17 21:26:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps! Your Node.js app is running.');
+  res.send('Hello from My DevOps! Your Node.js app is running.');
 });
 
 app.listen(port, () => {
